865b8f4b85ac18e31735bf6941b1cc719de186b4
Wojtek Nowak <billnewmann@gmail.com>
2020-07-28 22:56:00 +0100

    relation between employees and department collection

--- models/employees.model.js
+++ models/employees.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const employeesSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  department: { type: String, required: true }
+  department: { type: String, required: true, ref: 'Department' }
 });
 
 module.exports = mongoose.model('Employees', employeesSchema);
--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -6,7 +6,7 @@ const Employee = require('../models/employees.model');
 //get 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -20,7 +20,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const dep = await Employee.findOne().skip(rand);
+    const dep = await Employee.findOne().populate('department').skip(rand);
     if(!dep) res.status(404).json({ message: 'Not found' });
     else res.json(dep);
   }
@@ -34,7 +34,7 @@ router.get('/employees/random', async (req, res) => {
 router.get('/employees/:id', async (req, res) => {
 
   try {
-    const dep = await Employee.findById(req.params.id);
+    const dep = await Employee.findById(req.params.id).populate('department');
     if(!dep) res.status(404).json({ message: 'Not found' });
     else res.json(dep);
   }
