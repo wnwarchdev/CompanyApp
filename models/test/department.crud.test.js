3a8f74d1cb8f8c3d31621aaeab07d35c1996cedb
Wojtek Nowak <billnewmann@gmail.com>
2020-08-02 13:15:27 +0100

    Setup connection with fakeDB; add onchange and modify package.json

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -1,10 +1,27 @@
 const Department = require('../department.model');
 const expect = require('chai').expect;
+const MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
+const mongoose = require('mongoose');
 
 describe('Department', () => {
 
-  after(() => {
-    mongoose.models = {};
-  });
+    before(async () => {
+
+        try {
+        const fakeDB = new MongoMemoryServer();
+
+        const uri = await fakeDB.getConnectionString();
+
+        mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        } catch(err) {
+        console.log(err);
+        }
+
+    });
+
+    after(() => {
+      mongoose.models = {};
+    });
 
 });
